00b61b9a4411b7b11270a2d5e64715947d6a2acd
LSExiLio <sandoval.lucasj@gmail.com>
2019-06-15 03:37:10 -0300

    Fix - a initial test and config localhost as valid host

--- alojamiento/tests.py
+++ alojamiento/tests.py
@@ -1,10 +1,22 @@
 from django.test import TestCase
+from .models import Alojamiento
 
-class AnimalTestCase(TestCase):
+class AlojamientoTests(TestCase):
 
-    def setUp(self):
-      pass
+  def setUp(self):
+    Alojamiento.objects.create(
+      state="State",
+      town="Town",
+      address="Address",
+      type="Type",
+      category="Category"
+    )
 
-    def test_empty_set_should_be_true(self):
-        """test_empty_set_should_be_true"""
-        self.assertEqual(True, True)
+  def test_empty_set_should_be_true(self):
+    sut = Alojamiento.objects.get(state="State")
+    self.assertEqual(sut.town, "Town")
+    self.assertEqual(sut.address, "Address")
+    self.assertEqual(sut.type, "Type")
+    self.assertEqual(sut.category, "Category")
+    self.assertEqual(sut.state, "State")
+  
--- alojamientos_arqsoft_back_python_2019/settings.py
+++ alojamientos_arqsoft_back_python_2019/settings.py
@@ -27,7 +27,10 @@ SECRET_KEY = '0d^7)e5!m!al9er5753ok&9070@y81zbx#m&q=gz(5*b9ju31-'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['alojamiento-backend.herokuapp.com']
+ALLOWED_HOSTS = [
+    'alojamiento-backend.herokuapp.com',
+    'localhost'
+]
 
 CORS_ORIGIN_ALLOW_ALL = True
 
