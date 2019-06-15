38917695e8a7beed3e997a52763a401cb4db821d
LSExiLio <sandoval.lucasj@gmail.com>
2019-06-15 01:58:50 -0300

    Fix - settings to use sqlite

--- alojamientos_arqsoft_back_python_2019/settings.py
+++ alojamientos_arqsoft_back_python_2019/settings.py
@@ -80,14 +80,14 @@ WSGI_APPLICATION = 'alojamientos_arqsoft_back_python_2019.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    #'default': {
-    #    'ENGINE': 'django.db.backends.sqlite3',
-    #    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    #}
     'default': {
-        'ENGINE': 'djongo',
-        'NAME': 'myapp-djongo-db',
+       'ENGINE': 'django.db.backends.sqlite3',
+       'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+    # 'default': {
+    #     'ENGINE': 'djongo',
+    #     'NAME': 'myapp-djongo-db',
+    # }
 }
 
 
