16ee9f6291e0d6eef61b6b14ccd9ab7841f58dc4
LSExiLio <sandoval.lucasj@gmail.com>
2019-06-15 01:03:09 -0300

    Add - travis ci config

--- alojamiento/tests.py
+++ alojamiento/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
 
-# Create your tests here.
+class AnimalTestCase(TestCase):
+
+    def setUp(self):
+      pass
+
+    def test_empty_set_should_be_true(self):
+        """test_empty_set_should_be_true"""
+        self.assertEqual(True, True)
