2f8ded866492e82584a54cefa71b3f490fd29759
Guido Pujadas <guidopujadas@gmail.com>
2019-07-21 13:27:33 -0300

    ADD - user registration

--- accounts/views.py
+++ accounts/views.py
@@ -4,18 +4,31 @@ from django.contrib.auth import authenticate,login
 from django.contrib import messages
 from django.http import HttpResponseServerError,HttpResponse
 from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.models import User
+import logging
+import json
 # Create your views here.
 
 @csrf_exempt
 def registerUser(request):
+    logger = logging.getLogger(__name__)
+    
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        data = request.body.decode('utf-8')
+        received_json_data = json.loads(data)
+        form = UserCreationForm(received_json_data)
         if form.is_valid():
-            #username = form.cleaned_data['username']
-            form.save(commit=False)
+            logger.error("is Valid")
+            username = form.cleaned_data['username']
+            #email = form.cleaned_data['email']
+            password = form.cleaned_data['password1']
+            user = User.objects.create_user(username)
+            user.set_password(password)
+            user.save()
             #messages.success(request, f'Usuario {username} creado correctamente')
             return HttpResponse("Usuario creado correctamente")
         else:
+            logger.error(form.errors)
             return HttpResponseServerError(form.errors)
     else:
         return HttpResponse("nada")
