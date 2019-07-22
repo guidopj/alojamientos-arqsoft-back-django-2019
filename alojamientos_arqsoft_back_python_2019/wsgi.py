59ac70cefd591d4e2e25ba1c56e6e90e5ddfd98f
Guido Pujadas <Guido_Pujadas@itresources.com.ar>
2019-07-22 12:32:36 -0300

    make it better

--- alojamientos_arqsoft_back_python_2019/wsgi.py
+++ alojamientos_arqsoft_back_python_2019/wsgi.py
@@ -6,11 +6,3 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
-
-import os
-
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'alojamientos_arqsoft_back_python_2019.settings')
-
-application = get_wsgi_application()
